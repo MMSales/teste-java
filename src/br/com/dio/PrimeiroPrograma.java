f14c20793c6097965d6370028c1e189521547f5c
MMSales <mmselas02@gmail.com>
2021-08-15 14:00:26 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+
+/*ctrl + shift + seta: navega a linha pelo código */
+/* ctrl + d: duplicar linha */
+/* orgarnizar o código Ctrl + alt + L */
+/*ctrl + shift + F10 pra começar a rodar o programa */
+/* ctrl + shift + barra comenta a porra toda */
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+
+
+      /*  int a = 5;
+        int b = 3;
+        System.out.println("Olá, Mundo! " + (a+b));*/
+    }
+}
